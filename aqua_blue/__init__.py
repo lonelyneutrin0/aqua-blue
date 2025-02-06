c08c18f387ef943ff988f7c4db782b99c024e1c4
jwjeffr <jwjeffries2000@gmail.com>
2025-02-06 12:59:01 -0500

    pyproject.toml, fixes #6

--- aqua_blue/__init__.py
+++ aqua_blue/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __authors__ = ["Jacob Jeffries", "Ameen Mahmood"]
 __author_emails__ = ["jacob.jeffries@chicagoclubteam.org", "ameen.mahmood@chicagoclubteam.org"]
 __url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
