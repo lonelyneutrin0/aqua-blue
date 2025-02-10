f6f9aa26a5f363af8b6f6a4775f61dcfbfc97bac
Avik Thumati <49382938+aronavik@users.noreply.github.com>
2025-02-10 15:39:15 -0500

    Update time_series.py

--- aqua_blue/time_series.py
+++ aqua_blue/time_series.py
@@ -1,20 +1,16 @@
 from typing import IO, Union
 from pathlib import Path
-
 from dataclasses import dataclass
 import numpy as np
-
 from numpy.typing import NDArray
 
 
 @dataclass
 class TimeSeries:
-
     dependent_variable: NDArray
     times: NDArray
 
     def __post_init__(self):
-
         timesteps = np.diff(self.times)
         if not np.isclose(np.std(timesteps), 0.0):
             raise ValueError("TimeSeries.times must be uniformly spaced")
@@ -32,12 +28,10 @@ class TimeSeries:
 
     @property
     def num_dims(self) -> int:
-
         return self.dependent_variable.shape[1]
 
     @classmethod
     def from_csv(cls, fp: Union[IO, str, Path], time_index: int = 0):
-
         data = np.loadtxt(fp, delimiter=",")
         return cls(
             dependent_variable=np.delete(data, obj=time_index, axis=1),
@@ -46,24 +40,46 @@ class TimeSeries:
 
     @property
     def timestep(self) -> float:
-
         return self.times[1] - self.times[0]
 
     def __eq__(self, other) -> bool:
-
         return bool(np.all(self.times == other.times) and np.all(
             np.isclose(self.dependent_variable, other.dependent_variable)
         ))
 
-    def __add__(self, other):
+    def __getitem__(self, key):
+        """Enables slicing like time_series[:n]"""
+        return TimeSeries(self.dependent_variable[key], self.times[key])
 
-        if not len(self.times) == len(other.times):
-            raise ValueError("can only add TimeSeries instances that have the same number of timesteps")
+    def __setitem__(self, key, value):
+        """Allows modifying slices: time_series[:n] = new_time_series"""
+        if not isinstance(value, TimeSeries):
+            raise TypeError("Value must be a TimeSeries object")
 
-        if not np.all(self.times == other.times):
-            raise ValueError("can only add TimeSeries instances that span the same times")
+        if isinstance(key, slice):
+            if key.stop is not None and key.stop > len(self.dependent_variable):
+                raise ValueError("Slice stop index out of range")
+        elif isinstance(key, int):
+            if key >= len(self.dependent_variable):
+                raise ValueError("Index out of range")
 
-        return TimeSeries(
-            dependent_variable=self.dependent_variable + other.dependent_variable,
-            times=self.times
-        )
+        self.dependent_variable[key] = value.dependent_variable
+        self.times[key] = value.times
+
+    def __delitem__(self, key):
+        """Allows deleting slices: del time_series[:n]"""
+        if isinstance(key, slice):
+            if key.stop is not None and key.stop > len(self.dependent_variable):
+                raise ValueError("Slice stop index out of range")
+
+            mask = np.ones(len(self.dependent_variable), dtype=bool)
+            mask[key] = False
+            self.dependent_variable = self.dependent_variable[mask]
+            self.times = self.times[mask]
+
+        elif isinstance(key, int):
+            if key >= len(self.dependent_variable):
+                raise ValueError("Index out of range")
+
+            self.dependent_variable = np.delete(self.dependent_variable, key, axis=0)
+            self.times = np.delete(self.times, key, axis=0)
