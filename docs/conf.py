c1ed82759d5d577df1ef8368b79ea5ef85f4030b
jwjeffr <jwjeffries2000@gmail.com>
2025-02-11 00:25:24 -0500

    version and author fields now pull from module in sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -8,13 +8,15 @@
 
 import os
 import sys
+from aqua_blue import __version__, __authors__
 
 sys.path.insert(0, os.path.abspath(".."))
 
 project = 'aqua-blue'
-copyright = '2025, CCMC'
-author = 'CCMC'
-release = '0.0.15'
+copyright = '2025, Chicago Club Management Company'
+author = ", ".join(__authors__)
+release = __version__
+version = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,8 +25,6 @@ extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
