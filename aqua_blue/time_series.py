71d3040e2e716605ebd293a844edaf1dd9795ebd
Avik Thumati <aronavik@gmail.com>
2025-02-11 17:12:47 -0500

    merge conflict resolving

--- aqua_blue/time_series.py
+++ aqua_blue/time_series.py
@@ -21,9 +21,24 @@ class TimeSeries:
         if np.isclose(np.mean(timesteps), 0.0):
             raise ValueError("TimeSeries.times must have a timestep greater than zero")
 
-    def save(self, file: IO, header="", delimiter=","):
+    def save(self, fp: Union[IO, str, Path], header: str = "", delimiter=","):
+
+        """
+        Method to save a time series
+
+        Args:
+            fp (Union[IO, str, Path]):
+                The file-like object, path name, or Path in which to save the TimeSeries instance
+
+            header (str):
+                An optional header. Defaults to the empty string
+
+            delimiter (str):
+                The delimiting character in the save file. Defaults to a comma
+
+        """
         np.savetxt(
-            file,
+            fp,
             np.vstack((self.times, self.dependent_variable.T)).T,
             delimiter=delimiter,
             header=header,
@@ -33,10 +48,31 @@ class TimeSeries:
     @property
     def num_dims(self) -> int:
 
+        """
+        The dimensionality of the time series
+
+        Returns:
+            int: The dimensionality of the time series
+        """
+
         return self.dependent_variable.shape[1]
 
     @classmethod
     def from_csv(cls, fp: Union[IO, str, Path], time_index: int = 0):
+
+        """
+        Method for loading in a TimeSeries instance from a comma-separated value (csv) file
+
+        Args:
+            fp (Union[IO, str, Path]):
+                The file-like object, path name, or Path in which to read
+
+            time_index (int):
+                The column index corresponding to the time column. Defaults to 0
+
+        Returns:
+            TimeSeries: A TimeSeries instance populated by data from the csv file
+        """
 
         data = np.loadtxt(fp, delimiter=",")
 
@@ -47,6 +83,14 @@ class TimeSeries:
 
     @property
     def timestep(self) -> float:
+
+        """
+        The physical timestep of the time series
+
+        Returns:
+            int: The physical timestep of the time series
+        """
+
         return self.times[1] - self.times[0]
 
     def __eq__(self, other) -> bool:
@@ -55,25 +99,23 @@ class TimeSeries:
         ))
 
     def __getitem__(self, key):
-        """Enables slicing like time_series[:n]"""
+
         return TimeSeries(self.dependent_variable[key], self.times[key])
 
     def __setitem__(self, key, value):
-        """Allows modifying slices: time_series[:n] = new_time_series"""
+
         if not isinstance(value, TimeSeries):
             raise TypeError("Value must be a TimeSeries object")
-
-        if isinstance(key, slice):
-            if key.stop is not None and key.stop > len(self.dependent_variable):
-                raise ValueError("Slice stop index out of range")
-        elif isinstance(key, int):
-            if key >= len(self.dependent_variable):
-                raise ValueError("Index out of range")
+        if isinstance(key, slice) and key.stop > len(self.dependent_variable):
+            raise ValueError("Slice stop index out of range")
+        if isinstance(key, int) and key >= len(self.dependent_variable):
+            raise ValueError("Index out of range")
 
         self.dependent_variable[key] = value.dependent_variable
         self.times[key] = value.times
 
     def __add__(self, other):
+
         if not len(self.times) == len(other.times):
             raise ValueError("can only add TimeSeries instances that have the same number of timesteps")
 
@@ -86,6 +128,7 @@ class TimeSeries:
         )
 
     def __sub__(self, other):
+
         if not len(self.times) == len(other.times):
             raise ValueError("can only subtract TimeSeries instances that have the same number of timesteps")
 
@@ -97,8 +140,8 @@ class TimeSeries:
             times=self.times
         )
 
-
     def __rshift__(self, other):
+
         if self.times[-1] >= other.times[0]:
             raise ValueError("can only concatenate TimeSeries instances with non-overlapping time values")
 
