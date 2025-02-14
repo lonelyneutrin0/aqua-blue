6e82fe2fb197763158c31bf4c7a24168f5e8247a
jwjeffr <jwjeffries2000@gmail.com>
2025-02-13 22:21:46 -0500

    added type hinting formatting special cases, addresses #8

--- docs/conf.py
+++ docs/conf.py
@@ -21,13 +21,29 @@ version = __version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 autodoc_member_order = "bysource"
+typehints_fully_qualified = True
+
+
+def typehints_formatter(x, y=None):
+
+    try:
+        class_name = x.__name__
+    except AttributeError:
+        class_name = str(x)
+
+    return class_name. \
+        replace("[typing.Any, numpy.dtype[+_ScalarType_co]]", ""). \
+        replace("numpy.ndarray", "numpy.typing.NDArray"). \
+        replace("ndarray", "numpy.typing.NDArray"). \
+        replace("numpy.random._generator.Generator", "numpy.random.Generator")
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
