1119f65c301563f4548db37612f51b042996b421
jwjeffr <jwjeffries2000@gmail.com>
2025-03-20 19:37:03 -0400

    more exposition in examples

--- aqua_blue/__init__.py
+++ aqua_blue/__init__.py
@@ -1,27 +1,37 @@
-"""
+r"""
 .. include:: ../README.md
 
 # Examples
 
-## Basic Lotka-Volterra example
+## 🐇 Basic Lotka-Volterra example
+
+Below is an example of using `aqua-blue` to predict the Lotka-Volterra equations with initial parameters $\beta = 0.02$,
+$\gamma = 0.3$, and $\delta = 0.01$, and initial parameters $(x_0, y_0) = (20, 9)$. We train a reservoir computer
+with a dimensionality of $1000$ over $0\leq t\leq 10$, with $1000$ timesteps.
 
 ```py
 .. include:: ../examples/lotka-volterra.py
 ```
 
-## Using datetime objects
+## 🕓 Using datetime objects
+
+Below is an example of a simple sine-cosine task similar to above, using `datetime.datetime` objects as `times`.
 
 ```py
 .. include:: ../examples/sine-cosine.py
 ```
 
-## Load and output a JSON string
+## 📡 Load and output a JSON string
+
+Below is an example of inputting a `json` string as the training data, and outputting a `json` string for the
+prediction. This is particularly useful for interfacing `aqua-blue` with already-existing systems.
+
 ```py
 .. include:: ../examples/json_example.py
 ```
 """
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 __authors__ = [
     "Jacob Jeffries",
     "Hrishikesh Belagali",
